c8176b94f98e8eddac5f32c1cca066cace6ecceb
Parth Goel <goelparth5331@gmail.com>
2024-04-08 05:10:21 +0530

    Store model created

---
+++ src/Models/store.model.js
@@ -0,0 +1,27 @@
+import { Schema,model } from mongoose;
+import { pointSchema } from "../Utils/Schema.js";   
+
+const storeSchema = new Schema({
+    location:{
+        type:pointSchema,
+        index:"2dsphere",
+    },
+    manager:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    Inventory:[{
+        medicine:{
+            type:Schema.Types.ObjectId,
+            ref:"Medicine",
+        },
+        quantity:{
+            type:Number,
+            required:true,
+        }
+    }]
+
+},{timestamps:true});
+
+export const Store = model("Store",storeSchema);
