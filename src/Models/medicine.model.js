cd72124c70ac5505f6b3b95e66b356233ed7b0fc
Parth Goel <goelparth5331@gmail.com>
2024-04-08 05:26:17 +0530

    medicine model created

---
+++ src/Models/medicine.model.js
@@ -0,0 +1,41 @@
+import {Schema,model} from "mongoose";
+
+const medicineSchema = new Schema({
+    name:{ //compositon
+        type:String,
+        required:true,
+        trim:true,
+        lowercase:true,
+        index:true,
+    },
+    manufacturer:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    displayImages:[{
+        type:String,//cloudinary urls 
+    }],
+    categories:[{
+        type:Schema.Types.ObjectId,
+        ref:"Category",
+    }],
+    description:{
+        type:String, //might get a link from timyMCE
+        required:true,
+    },
+    expiration:{
+        type:Date,
+        required:true,
+    },
+    reviews:[{
+        type:Schema.Types.ObjectId,
+        ref:"Review",
+    }]
+    
+},{timestamps:true});
+export const Medicine  = model("Medicine",medicineSchema);
