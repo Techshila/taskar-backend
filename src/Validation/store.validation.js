f15a1747603fb6192689d1c03b5f8b653a31dcc6
Parth Goel <goelparth5331@gmail.com>
2024-04-08 08:10:23 +0530

    Store Validator made

---
+++ src/Validation/store.validation.js
@@ -0,0 +1,24 @@
+import {z} from 'zod';
+ 
+
+const storeValidator = z.object({
+    location:z.object({
+        type:z.enum(["Point"]),
+        coordinates:z.array(z.number())
+        .length(2,{message:"Invalid Coordinates"}),
+    }),
+    manager:z.string()
+            .refine(val=> mongoose.Types.ObjectId.isValid(val),
+                    val=>({message:`Invalid User Id for Manager: ${val}`})),
+    Inventory:z.array(
+        z.object({
+            medicine:z.string()
+                    .refine(val=> mongoose.Types.ObjectId.isValid(val),
+                            val=>({message:`Invalid Medicine Id: ${val}`})),
+            quantity:z.coerce.number({required_error:"Quantity is required"})
+                    .positive({message:"Quantity must be positive"})
+        })
+    ),
+  
+})
+export {storeValidator};
