4489a56dc7b03519f56b2afd3b5d2440caa334ce
Parth Goel <goelparth5331@gmail.com>
2024-04-08 07:19:25 +0530

    made zod util functions to help modify validators

---
+++ src/Utils/Zod.js
@@ -0,0 +1,58 @@
+import {z} from Zod;
+import ApiError from "./ApiError.js";
+
+
+const makePartialValidatorByPickingKeys = (zodValidator,keys) => {
+    if(keys.length==0 ){
+        return schema.deepPartial(); // all fields are optional at nested level 
+    };
+    const arrayOfKeysOfPartialValidator  = zodValidator.keyof().options;
+  
+    let options = {};
+    
+    for (let key of keys){
+        if(arrayOfKeysOfPartialValidator.includes(key)){
+            options[String(key)] = true;
+        }else{
+            throw new ApiError(400,`Invalid User field choosen to make partial zodSchema: ${key}`);
+        }
+    }
+    return zodValidator.pick(options);
+};
+const makePartialValidatorByOmmittingKeys = (zodValidator,keys) => {
+    if(keys.length==0 ){
+        return schema.deepPartial(); // all fields are optional at nested level 
+    };
+    const arrayOfKeysOfPartialValidator  = zodValidator.keyof().options;
+  
+    let options = {};
+    
+    for (let key of keys){
+        if(arrayOfKeysOfPartialValidator.includes(key)){
+            options[String(key)] = true;
+        }else{
+            throw new ApiError(400,`Invalid User field choosen to make partial zodSchema: ${key}`);
+        }
+    }
+    return zodValidator.omit(options);
+};
+
+const makefieldsOptionalInValidator = (zodValidator,keys) => {
+    if(keys.length==0 ){
+        return schema.deepPartial(); // all fields are optional at nested level 
+    };
+    const arrayOfKeysOfPartialValidator  = zodValidator.keyof().options;
+  
+    let options = {};
+    
+    for (let key of keys){
+        if(arrayOfKeysOfPartialValidator.includes(key)){
+            options[String(key)] = true;
+        }else{
+            throw new ApiError(400,`Invalid User field choosen to make partial zodSchema: ${key}`);
+        }
+    }
+    return zodValidator.partial(options);
+
+};
+export {makePartialValidatorByPickingKeys,makePartialValidatorByOmmittingKeys,makefieldsOptionalInValidator};
