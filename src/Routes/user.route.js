e725fbd0b85607b90d92ade3ce899420906b39f9
Vaibhav-Bothra <vaibhavbothra2004@gmail.com>
2024-04-09 23:47:25 +0530

    Feat: changes for createreview route.

--- src/Routes/user.route.js
+++ src/Routes/user.route.js
@@ -11,6 +11,6 @@ router.get('/add/:idx',cartController.addqty);
 router.get('/subtract/:idx',cartController.subtractqty);
 router.get('/del/:idx',cartController.del);
 
-router.post('./createreview',userController.createreview);
+router.post('/createreview',userController.createreview);
 
 module.exports = router;
