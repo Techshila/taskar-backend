1619768accca764a5f4d6ba51ee4c48fdb323308
Parth Goel <goelparth5331@gmail.com>
2024-04-11 16:00:31 +0530

    search medicine route tested

--- src/Controllers/search.controller.js
+++ src/Controllers/search.controller.js
@@ -5,11 +5,12 @@ const search = async function(req,res){
     let searchItem = req.params.id;
     console.log(searchItem);
     let a = [];
-    Medicine.find({}).then((ele) => {
+    await Medicine.find({}).then((ele) => {
         if(ele){
             for(let i=0;i<ele.length;i++){
                 let medi = ele[i];
                 let name = medi.name.toLowerCase();
+                console.log(name);
                 if(name.includes(searchItem)){
                     a.push(ele[i]);
                 }
