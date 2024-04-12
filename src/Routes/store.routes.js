e209caaf7a9f79fd2ad9a26dae88352ba9fea6c5
Parth Goel <goelparth5331@gmail.com>
2024-04-12 22:45:12 +0530

    controllers related to orders

--- src/Routes/store.routes.js
+++ src/Routes/store.routes.js
@@ -1,7 +1,8 @@
 import {Router} from "express";
-import { getNearestStore ,registerStore,verifyStore,storeManagerLogin, addToInventory, deleteFromInventory, updateInventory, fetchVerifiedStore, fetchUnVerifiedStore } from "../Controllers/store.controller.js";
+import { getNearestStore ,registerStore,verifyStore,storeManagerLogin, addToInventory, deleteFromInventory, updateInventory, fetchVerifiedStore, fetchUnVerifiedStore ,fetchCompletedOrders} from "../Controllers/store.controller.js";
 import authMiddleware from "../Middlewares/auth.middleware.js";
 import { multerUpload } from "../Middlewares/multer.middleware.js";
+
 const router = Router();
 
 router.get("/getNearestStore",getNearestStore) ;
@@ -16,7 +17,7 @@ router.post("/addInventory",authMiddleware,addToInventory);
 router.post("/deleteInventory",authMiddleware,deleteFromInventory);
 router.post("/updateInventory",authMiddleware,updateInventory);
 
-
+router.post("/fetchCompletedOrders",fetchCompletedOrders);
 
 
 export default router;
