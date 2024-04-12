09e3911671f492479b3e64b3fedd1c809ae77374
Vaibhav-Bothra <vaibhavbothra2004@gmail.com>
2024-04-12 21:25:42 +0530

    Feat: added inventory routes.

--- src/Routes/store.routes.js
+++ src/Routes/store.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { getNearestStore ,registerStore,verifyStore,storeManagerLogin } from "../Controllers/store.controller.js";
+import { getNearestStore ,registerStore,verifyStore,storeManagerLogin, addToInventory, deleteFromInventory, updateInventory } from "../Controllers/store.controller.js";
 import authMiddleware from "../Middlewares/auth.middleware.js";
 import { multerUpload } from "../Middlewares/multer.middleware.js";
 const router = Router();
@@ -10,6 +10,9 @@ router.post("/registerStore",authMiddleware,registerStore);
 router.post("/verifyStore",authMiddleware,verifyStore);
 router.post("/storeManagerLogin",multerUpload.none(),storeManagerLogin);
 
+router.post("/addInventory",authMiddleware,addToInventory);
+router.post("/deleteInventory",authMiddleware,deleteFromInventory);
+router.post("/updateInventory",authMiddleware,updateInventory);
 
 
 
